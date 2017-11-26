94d75b2d3deb85a0060f527912aa019e964b5475
svelandiac <31489941+svelandiac@users.noreply.github.com>
2017-11-26 02:17:53 -0500

    Fantasmas con inteligencia artificial
    Los fantasmas te persiguen

--- pacman/pacman.js
+++ pacman/pacman.js
@@ -1,6 +1,8 @@
 
 var user;
 var img;
+var redGhost;
+var yellowGhost;
 function preload() {
   img = loadImage("fondo.jpg");
 }
@@ -8,6 +10,9 @@ function setup() {
   createCanvas(1400, 800);
   noStroke();
   user = new pacman(822, 500);
+  redGhost = new ghost(819, 367, 255, 0, 0, 0);
+  yellowGhost = new ghost(819, 367, 255, 165, 0, 2);
+
 }
 
 function draw() {
@@ -15,13 +20,22 @@ function draw() {
   field();
   user.move(2);
   user.display();
-  console.log(mouseX, mouseY);
+  redGhost.display();
+  redGhost.move();
+  yellowGhost.display();
+  yellowGhost.move();
+  console.log(redGhost.goalX, redGhost.goalY);
   if(user.x >= 1133){
     user.x = 515;
   }
   if(user.x <= 509){
     user.x = 1133;
   }
+    redGhost.goalX = user.x;
+    redGhost.goalY = user.y;
+    yellowGhost.goalX = user.x;
+    yellowGhost.goalY = user.y;
+
 }
 function field(){
   push();
